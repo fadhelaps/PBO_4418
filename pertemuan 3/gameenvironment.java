751103abec6b4b05d8f8590dd17a2a2366f67f17
fadhelaps <80695114+fadhelaps@users.noreply.github.com>
2021-03-23 19:17:25 +0700

    Add files via upload

---
+++ pertemuan 3/gameenvironment.java
@@ -0,0 +1,10 @@
+package gameenvironment;
+public class gameenvironment {
+
+    public static void main(String[] args) {
+        gameplayer coba = new gameplayer();
+        coba.setPosition(30,20);
+        coba.run(60);
+        coba.run();
+    }
+}
